1c5a6d924a6772b15cf6344d8c822402b2ad9742
mikiyas <95138295+MIKIYAS2021@users.noreply.github.com>
2023-01-15 19:44:22 +0300

    Update FizzBuzz.py

--- FizzBuzz.py
+++ FizzBuzz.py
@@ -1,13 +1,13 @@
 class Solution:
     def fizzBuzz(self, n: int) -> List[str]:
-        lists = []
-        for x in range(1,n+1):
-            if x%15 == 0:
-                lists.append("FizzBuzz")
-            elif x%5 == 0:
-                lists.append("Buzz")
-            elif x%3 == 0:
-                lists.append("Fizz")
+        ans = []
+        for i in range(1,n+1):
+            if i%15 == 0:
+                ans.append("FizzBuzz")
+            elif i%3 == 0:
+                ans.append("Fizz")
+            elif i%5 == 0:
+                ans.append("Buzz")
             else:
-                lists.append(str(x))
-        return lists
+                ans.append(str(i))
+        return ans
