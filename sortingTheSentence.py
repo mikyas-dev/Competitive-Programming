39ae27f1075a2398c63c60ab52e6353966f6f49f
mikiyas <95138295+MIKIYAS2021@users.noreply.github.com>
2023-01-16 19:23:28 +0300

    Update sortingTheSentence.py

--- sortingTheSentence.py
+++ sortingTheSentence.py
@@ -1,23 +1,8 @@
-class Solution(object):
-    def sortSentence(self, s):
-        """
-        :type s: str
-        :rtype: str
-        """
+class Solution:
+    def sortSentence(self, s: str) -> str:
         words = s.split()
-        n = len(words)
-        correct=""
-        for i in range(0,n-1):
-            min=i
-            for j in range(i+1,n):
-                if words[j][len(words[j])-1]<words[min][len(words[min])-1]:
-                    min = j
-            if min !=i:
-                words[min],words[i]=words[i],words[min]
-        for x in range(0,n):
-            if x == n-1:
-                correct=correct+words[x][0:len(words[x])-1]
-            else:
-                correct=correct+words[x][0:len(words[x])-1]+" "
-        return correct
-        
+        ans = ""
+        words.sort(key = lambda x:x[-1])
+        for i in range(len(words)):
+            ans=ans+words[i][:-1]+" "
+        return ans[:-1]
