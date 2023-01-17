9726087c33b70398296d0d66d969fa84b0feed18
mikiyas <95138295+MIKIYAS2021@users.noreply.github.com>
2023-01-17 16:54:45 +0300

    Update SortColor.py

--- SortColor.py
+++ SortColor.py
@@ -3,11 +3,10 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-        for i in range(0,len(nums)-1):
-            min = i
-            for j in range(i+1,len(nums)):
-                if nums[j]<nums[min]:
-                    min = j
-            if min!=i:
-                nums[min],nums[i]=nums[i],nums[min]
-        
+        for i in range(1,len(nums)):
+            j=i-1
+            temp = nums[i]
+            while nums[j]>temp and j>=0:
+                nums[j+1]=nums[j]
+                j-=1
+            nums[j+1]=temp
