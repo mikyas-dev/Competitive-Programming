7aea3e4377c4341ca013d06b23d616df058a8439
mikiyas <95138295+MIKIYAS2021@users.noreply.github.com>
2023-01-20 19:19:27 +0300

    Update Theatre Square.py

--- Theatre Square.py
+++ Theatre Square.py
@@ -1,6 +1,6 @@
-import math
-inp=input()
-s =inp.split()
-m=math.ceil(int(s[0])/int(s[-1]))
-n=math.ceil(int(s[1])/int(s[-1]))
-print(m*n)
+    import math
+    inp=input()
+    s =inp.split()
+    m_update=math.ceil(int(s[0])/int(s[-1]))
+    n_update=math.ceil(int(s[1])/int(s[-1]))
+    print(m_update*n_update)
