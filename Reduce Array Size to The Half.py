d42a09974d605150ee63de9c78d204d5f155b3db
mikiyas <95138295+MIKIYAS2021@users.noreply.github.com>
2023-01-20 18:52:22 +0300

    Update Reduce Array Size to The Half.py

--- Reduce Array Size to The Half.py
+++ Reduce Array Size to The Half.py
@@ -1,22 +1,13 @@
 class Solution:
     def minSetSize(self, arr: List[int]) -> int:
-        di={}
-        for i in arr:
-            if i in di:
-                di[i]+=1
-            else:
-                di[i]=1
-        di=sorted(di.items(), key =
-             lambda item:item[1])
-        di=dict(di)
-        col=0
-        ans=0
-        li=list(di.keys())
-        q=len(di)-1
-        for j in range(len(di)):
-            x=li[q-j]
-            col+=di[x]
-            ans+=1
-            if col>=len(arr)//2:
+        ans=sorted(Counter(arr).items(), key= lambda x:x[1],reverse=True)
+        res=tem=0
+        for i in ans:
+            tem+=i[1]
+            res+=1
+            if tem>=len(arr)//2:
                 break
-        return ans
+        return res
+            
+
+            
