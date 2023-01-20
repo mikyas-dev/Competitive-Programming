c8e472db53f79bee832f3b97c70e12f1d579a280
mikiyas <95138295+MIKIYAS2021@users.noreply.github.com>
2023-01-20 16:44:19 +0300

    Update Minimize Maximum Pair Sum in Array.py

--- Minimize Maximum Pair Sum in Array.py
+++ Minimize Maximum Pair Sum in Array.py
@@ -2,9 +2,9 @@ class Solution:
     def minPairSum(self, nums: List[int]) -> int:
         nums.sort()
         i,j=0,len(nums)-1
-        ans=[]
-        for m in range(len(nums)//2):
-            ans.append(nums[i]+nums[j])
-            i+=1
-            j-=1
-        return max(ans)
+        ans=0
+        while i<j:
+            if ans < nums[i]+nums[j]:
+                ans = nums[i]+nums[j]
+            i,j=i+1,j-1
+        return ans
