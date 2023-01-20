c27e7e3ed8e06b2e531f43ed10b70052b5885ba9
mikiyas <95138295+MIKIYAS2021@users.noreply.github.com>
2023-01-20 19:13:35 +0300

    Update number of good pairs.py

--- number of good pairs.py
+++ number of good pairs.py
@@ -1,16 +1,4 @@
 class Solution:
     def numIdenticalPairs(self, nums: List[int]) -> int:
-        dic={}
-        nums.sort()
-        for i in nums:
-            if not i in dic:
-                dic[i]=1
-            else:
-                dic[i]=dic[i]+1
-        lis=dic.values()
-        ans=0
-        for i in lis:
-            if i>1:
-                for j in range(i):
-                    ans+=j
-        return ans
+        return int(sum(k * (k - 1) / 2 for k in Counter(nums).values()))
+        
