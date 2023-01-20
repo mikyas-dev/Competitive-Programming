5eb382fafda416027e3dc758f5b6a10d6fee7933
mikiyas <95138295+MIKIYAS2021@users.noreply.github.com>
2023-01-20 23:26:07 +0300

    Update validParentheses.py

--- validParentheses.py
+++ validParentheses.py
@@ -1,23 +1,11 @@
 class Solution:
     def isValid(self, s: str) -> bool:
-        lis=[s[0]]
-        boo=False
-        i,j=1,0
-        while i<len(s):
-            if not lis:
-                lis.append(s[i])
-                i+=1
-                j=0
-                continue
-            elif (lis[j]=="(" and s[i]==")") or (lis[j]=="{" and s[i]=="}") or (lis[j]=="[" and s[i]=="]"):
-                boo=True
-                lis.pop()
-                j-=1
+        stack=[]
+        for i in s:
+            if i in ('(','{','['):
+                stack.append(i)
+            elif stack and ((i==')' and stack[-1]=='(') or (i=='}' and stack[-1]=='{') or (i==']' and stack[-1]=='[')):
+                stack.pop()
             else:
-                j+=1
-                lis.append(s[i])
-                boo=False
-            i+=1
-        return boo and not lis
-                
-        
+                return False
+        return len(stack)==0
