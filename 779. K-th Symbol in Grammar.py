9a1af96a794e8140692b95941e7ed57614c38b38
mikiyas <95138295+MIKIYAS1221@users.noreply.github.com>
2023-03-10 09:27:00 +0300

    Create 779. K-th Symbol in Grammar.py

---
+++ 779. K-th Symbol in Grammar.py
@@ -0,0 +1,7 @@
+class Solution:
+    def kthGrammar(self, n: int, k: int) -> int:
+        if k == 1:
+            return 0
+        if k%2 == 0:
+            return 1 - self.kthGrammar(n-1,ceil(k/2))
+        return self.kthGrammar(n-1,ceil(k/2))
