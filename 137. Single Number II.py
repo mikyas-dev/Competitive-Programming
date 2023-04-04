068f4d9b9f3bf1770b9de268c8d5e20547a4faef
mikiyas <95138295+MIKIYAS1221@users.noreply.github.com>
2023-04-04 19:58:43 +0300

    Create 137. Single Number II.py

---
+++ 137. Single Number II.py
@@ -0,0 +1,8 @@
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+        one = two = 0
+        for num in nums:
+            one = (one ^ num) & ~two
+            two = (two ^ num) & ~one       
+        
+        return one
