b6614f22ecc0fe9bbf424ffc96810e9c53f9ee66
mikiyas <95138295+MIKIYAS1221@users.noreply.github.com>
2023-05-31 19:02:19 +0300

    added get-maximum-in-generated-array.python

--- get-maximum-in-generated-array.py
+++ get-maximum-in-generated-array.py
@@ -1,13 +1,25 @@
 class Solution:
+    def __init__(self):
+        self.memo = dict()
+        self.memo[0] = 0
+        self.memo[1] = 1
+        self.large = 0
+
     def getMaximumGenerated(self, n: int) -> int:
-        temp=[]
+        if n == 0:
+            return 0
+        
         for i in range(n+1):
-            if i==0:
-                temp.append(0)
-            elif i==1:
-                temp.append(1)
-            elif i%2==0:
-                temp.append(temp[i//2])
-            else:
-                temp.append(temp[i//2]+temp[i//2+1])
-        return max(temp)
+            self.dp(i)
+        # print(self.memo)
+        return max(self.memo.values())
+        
+    def dp(self,n):
+        if n in self.memo:
+            return self.memo[n]
+        if n %2==0:
+            self.memo[n] = self.dp(n//2)
+        else:
+            i = int(n/2)
+            self.memo[n] = self.dp(i+1)+ self.dp(i)
+        return self.memo[n]
