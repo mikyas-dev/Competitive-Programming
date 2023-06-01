efe652b19fab111210a5e4bc8fe8550853e02a13
mikiyas <95138295+MIKIYAS1221@users.noreply.github.com>
2023-06-01 17:35:59 +0300

    added min-cost-climbing-stairs.python

---
+++ min-cost-climbing-stairs.py
@@ -0,0 +1,12 @@
+class Solution:
+    def minCostClimbingStairs(self, cost: List[int]) -> int:
+        n = len(cost)
+        dp = [0] * (n + 1)
+
+        dp[0] = cost[0]
+        dp[1] = cost[1]
+
+        for i in range(2, n + 1):
+            dp[i] = min(dp[i-1], dp[i-2]) + cost[i] if i < n else min(dp[i-1], dp[i-2])
+
+        return dp[n]
