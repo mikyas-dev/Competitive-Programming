f848ef525963a71d12ea9754c7664d00724ebb7a
mikiyas <95138295+MIKIYAS1221@users.noreply.github.com>
2023-06-01 18:06:32 +0300

    added target-sum.python

--- target-sum.py
+++ target-sum.py
@@ -1,15 +1,17 @@
 class Solution:
     def findTargetSumWays(self, nums: List[int], target: int) -> int:
         
-        @cache
+        memo = {}
         def dp(idx , tot):
             if idx == len(nums):
                 if tot == 0:
                     return 1
                 return 0
-            
-            ways = dp(idx+1,tot-nums[idx])+dp(idx+1,tot+nums[idx])
+            ways = (idx,tot)
+            if ways not in memo:
+    
+                memo[ways] = dp(idx+1,tot-nums[idx])+dp(idx+1,tot+nums[idx])
 
-            return ways
+            return memo[ways]
         
         return dp(0 , target)
